c37009f9c87489ce6d93f3c1a57269932285e68e
Kapil Kumar1 <kapil.kumar1@olacabs.com>
2018-01-04 16:15:38 +0530

    car renting system

---
+++ src/AutomationProblemSolving/CarRentalSystem.java
@@ -0,0 +1,18 @@
+package AutomationProblemSolving;
+
+
+/*
+Design a car renting system, including reserving a car, checking in and checking out. consider all the cases: reserve a car, then check out successfully;
+ reserve a car, but the car is sold out before you check out...
+Add the test cases.
+*/
+
+
+
+public class CarRentalSystem {
+
+
+
+
+
+}
